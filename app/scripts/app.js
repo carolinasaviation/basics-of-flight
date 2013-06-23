a8575b591e5040605ce93d8469b086419191c868
Dustan Kasten <dustan.kasten@gmail.com>
2013-06-23 00:36:23 -0400

    Initial page manager

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,60 @@
 /*global define */
-define([], function () {
+define([
+	'./pages/forces',
+	'./pages/controls',
+	'./pages/preflight',
+	'./views/navigation',
+], function (forces, controls, preflight, viewNavigation) {
 	'use strict';
 
-	var ACTIVE_CLASS = 'nav-item-active';
-	document.querySelector('.nav').addEventListener('click', function(e) {
-		var active = document.querySelector('.nav .' + ACTIVE_CLASS);
-		if (active) active.classList.remove(ACTIVE_CLASS);
+	var NAV_ACTIVE_CLASS = 'nav-item-active';
 
-		var node = e.target;
+	function AppManager() {
+		this.pages = [
+			forces,
+			controls,
+			preflight
+		];
 
-		while (!node.matches('.nav-item'))
-			node = node.parentNode;
+		this.render();
+		this.bind();
+		this.init();
+	}
 
-		node.classList.add(ACTIVE_CLASS);
-		console.log('TODO: Toggle page manager for: ' + node.textContent);
-	}, false);
+	AppManager.prototype = {
+		init: function() {
+			document.querySelector('.nav-item').click();
+		},
 
-	return '\'Allo \'Allo!';
+		render: function() {
+			var nav = viewNavigation({ pages: this.pages.map(function(page) { return page.name; }) })
+			var tmp = document.createElement('div');
+			tmp.innerHTML = nav;
+			document.body.insertBefore(tmp.firstChild, document.body.firstChild);
+		},
+
+		bind: function() {
+			var pages = this.pages;
+			document.querySelector('.nav').addEventListener('click', function(e) {
+				var active = document.querySelector('.nav .' + NAV_ACTIVE_CLASS);
+				if (active) {
+					page = _.findWhere(pages, { name: active.textContent });
+					page.unload();
+					active.classList.remove(NAV_ACTIVE_CLASS);
+				}
+
+				var node = e.target;
+
+				while (!node.matches('.nav-item'))
+					node = node.parentNode;
+
+				node.classList.add(NAV_ACTIVE_CLASS);
+				var page = _.findWhere(pages, { name: node.textContent });
+				page.load();
+			}, false);
+		}
+	}
+
+
+	return new AppManager();
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
+        lodash: '../bower_components/lodash/lodash',
     },
     shim: {
         bootstrap: {
@@ -10,12 +11,13 @@ require.config({
     }
 });
 
-require(['app', 'jquery', 'polyfills'], function (app, $) {
+require(['app', 'jquery', 'lodash', 'polyfills'], function (app, $, _) {
 	'use strict';
+
 	// use app here
-	console.log(app);
-	console.log('Running jQuery %s', $().jquery);
-	
+	console.log('Running jQuery %s', $.prototype.jquery);
+	console.log('Running LoDash %s', _.VERSION);
+
 	// select the first page....sort of
-	document.querySelector('.nav-item').click()
+	//document.querySelector('.nav-item').click()
 });
