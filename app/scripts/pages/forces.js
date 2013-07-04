5aa971b3d3e367234224687c7b9fefaa73632e15
Dustan Kasten <dustan.kasten@gmail.com>
2013-07-04 14:29:11 -0400

    Basic paper.js animation on forces pages

--- app/scripts/pages/forces.js
+++ app/scripts/pages/forces.js
@@ -39,6 +39,12 @@ define([
 		this.element.appendChild(this.card);
 		this.element.appendChild(config.createDomNode(html));
 
+		var canvas = this.canvas = document.createElement('canvas');
+		canvas.setAttribute('data-paper-resize', 'true');
+		canvas.style.position = 'absolute';
+		canvas.style.top = 0;
+		canvas.style.left = 0;
+
 		weight.page(this);
 		lift.page(this);
 		drag.page(this);
@@ -52,10 +58,56 @@ define([
 
 		draw.createAnimation(element, '3s linear infinite', [
 		 [0, '-webkit-transform: translate(0,0);'],
-		 [25, '-webkit-transform: translate(0,30px)'],
+		 [27, '-webkit-transform: translate(0,30px)'],
 		 [50, '-webkit-transform: translate(0,0px)'],
-		 [75, '-webkit-transform: translate(0,-30px)']
+		 [73, '-webkit-transform: translate(0,-30px)']
 		]);
+
+		// temp!
+		this.activate();
+	};
+
+	Forces.prototype.activate = function() {
+		Page.prototype.activate.call(this);
+		this.element.appendChild(this.canvas);
+
+		if (window.view && view._element === this.canvas) return;
+
+		paper.install(window);
+		paper.setup(this.canvas);
+		var num = 100;
+		circles = new Array(100);
+		while (num--)
+			circles[num] = new paper.Path.Circle({
+					center: [rand(-10, config.width), rand(-10, config.height)],
+					radius: rand(3, 6),
+					fillColor: new paper.Color(255,255,255),
+					strokeColor: new paper.Color(255,255,255,0.3),
+					strokeWidth: 5
+				});
+
+		view.onFrame = onFrame;
+		var w = view.viewSize.width;
+		var h = view.viewSize.height;
+		function onFrame(event) {
+			circles.forEach(function(c, i) {
+				if (c.position.x > w) c.position.x = -10;
+				if (c.position.y < 0) c.position.y = h + 10;
+				c.position.x += rand(3, 5);
+				c.position.y -= rand(1, 3);
+			});
+		}
+	};
+
+	Forces.prototype.deactivate = function() {
+		Page.prototype.deactivate.call(this);
+		project.remove();
+		this.element.removeChild(this.canvas);
+	};
+
+	function rand(min, max, isFloat) {
+		var rand = Math.random();
+		return Math.floor(max * rand) + min;
 	}
 
 	return new Forces();
