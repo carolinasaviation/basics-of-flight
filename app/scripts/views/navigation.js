ea2976995884e7006d66cbb4c95ef6867c881ce1
Dustan Kasten <dustan.kasten@gmail.com>
2013-07-23 22:38:23 -0400

    Downcase the navigation image

--- app/scripts/views/navigation.js
+++ app/scripts/views/navigation.js
@@ -15,7 +15,7 @@ define(['lodash'], function(_) {
 				{[ _.forEach(pages, function(page) { ]}
 					<li class="nav-item-container">
 						<a class="nav-item" href="#/{{ page.name }}">
-							<img class="nav-image" src="images/proto-nav-{{ page.name }}.png">
+							<img class="nav-image" src="images/proto-nav-{{ page.name.toLowerCase() }}.png">
 							<span class="nav-text">{{ page.name }}</span>
 						</a>
 						<ul class="subnav">
